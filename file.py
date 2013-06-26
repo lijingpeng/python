b220728e179615993ba4c6e6d504540e146d099e
lijingpeng <lijingpeng@outlook.com>
2013-06-26 09:47:44 +0800

    open file

---
+++ file.py
@@ -0,0 +1,9 @@
+# python file operation
+
+# 1. open file
+fread = open('/home/lijingpeng/test.txt')
+fwrite = open('/home/lijingpeng/test.txt', 'w')
+frw = open('/home/lijingpeng/test.txt', 'r+')
+frb = open('/home/lijingpeng/test.txt', 'rb')
+
+
