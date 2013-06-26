7360e209221a8b7fd92b205c483c952988d17a05
lijingpeng <lijingpeng@outlook.com>
2013-06-26 10:29:24 +0800

    read the file

--- file.py
+++ file.py
@@ -1,13 +1,22 @@
 # python file operation
 
+filename = '/home/lijingpeng/test.txt'
 # 1. open file
-fread = open('/home/lijingpeng/test.txt')
-fwrite = open('/home/lijingpeng/test.txt', 'w')
-frw = open('/home/lijingpeng/test.txt', 'r+')
-frb = open('/home/lijingpeng/test.txt', 'rb')
+fread = open(filename)
+# fwrite = open(filename, 'w')
+# frw = open(filename, 'r+')
+# frb = open(filename, 'rb')
 
+# 2. read the file
+done = 0
+while not done:
+	sLine = fread.readline()
+	if sLine != "":
+		print sLine
+	else:
+		done = 1
 # close all the file operater
 fread.close();
-fwrite.close();
-frw.close();
-frb.close();
+# fwrite.close();
+# frw.close();
+# frb.close();
