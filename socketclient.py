e9aa076f87cd05e331f8d56f12bed0d5fea8e99e
lijingpeng <lijingpeng@outlook.com>
2013-06-26 21:35:24 +0800

    finished the basic socket communication

--- socketclient.py
+++ socketclient.py
@@ -1,3 +1,25 @@
 #!/usr/bin/env python
 
 import socket
+
+HOST = 'localhost'
+PORT = 6666
+BUFSIZE = 1024
+ADDR = (HOST, PORT)
+
+# TCP socket
+tcpCliSoc = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+tcpCliSoc.connect(ADDR)
+
+while 1:
+    data = raw_input('>')
+    if not data: break
+    if data == 'exit': 
+        break
+        print 'Client close the connection'
+    tcpCliSoc.send(data)
+    data = tcpCliSoc.recv(BUFSIZE)
+    if not data: break
+    print data
+
+tcpCliSoc.close()
--- socketserver.py
+++ socketserver.py
@@ -1,2 +1,29 @@
 #!/usr/bin/env python
-import socket
+from socket import *
+from time import time, ctime
+from datetime import *
+
+HOST = ''
+PORT = 6666
+BUFSIZE = 1024
+ADDR = (HOST, PORT)
+
+# TCP server
+tcpSerSoc = socket()
+tcpSerSoc.bind(ADDR)
+tcpSerSoc.listen(5)
+
+while 1:
+    print 'Waiting for connection...'
+    tcpCliSoc, addr = tcpSerSoc.accept()
+    print addr, ' connected to server'
+
+    while 1:
+        data = tcpCliSoc.recv( BUFSIZE )
+        if not data: break
+        print data
+        tcpCliSoc.send("Welcome to the server")
+        #tcpCliSoc.send('Welcome to the server! ',time.strftime("%a %b %d %H:%M:%S", time.localtime()))
+    tcpCliSoc.close()
+    print addr, ' close the connection.'
+tcpSerSoc.close()
