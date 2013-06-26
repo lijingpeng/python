e988f30004d237a5fe236090422ee4ab5f25f277
lijingpeng <lijingpeng@outlook.com>
2013-06-26 23:01:51 +0800

    add try catch to tcp

--- tcpsocketserver.py
+++ tcpsocketserver.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 from socket import *
-from time import time, ctime
-from datetime import *
+from datetime import datetime
+import time
+import sys
 
 HOST = ''
 PORT = 6666
@@ -9,9 +10,18 @@ BUFSIZE = 1024
 ADDR = (HOST, PORT)
 
 # TCP server
-tcpSerSoc = socket()
-tcpSerSoc.bind(ADDR)
-tcpSerSoc.listen(5)
+try:
+    tcpSerSoc = socket()
+    tcpSerSoc.bind(ADDR)
+    tcpSerSoc.listen(5)
+except socket.error, msg:
+    print 'Socket Error! Create, bind, listen' + str(msg[0] + ', Error message:' + msg[1])
+    sys.exit()
+except Exception, errcode:
+    if errcode[0] == 10035:
+        print 'Error Code:10035'
+    if errcode[0] == 'timed out':
+        print 'Time out'
 
 while 1:
     print 'Waiting for connection...'
@@ -19,11 +29,31 @@ while 1:
     print addr, ' connected to server'
 
     while 1:
-        data = tcpCliSoc.recv( BUFSIZE )
+        try:
+            data = tcpCliSoc.recv( BUFSIZE )
+        except socket.error, msg:
+            print 'Socket Error! Recv' + str(msg[0] + ', Error message:' + msg[1])
+            sys.exit()
+        except Exception, errcode:
+            if errcode[0] == 10035:
+                print 'Error Code:10035'
+            if errcode[0] == 'timed out':
+                print 'Time out'
+
         if not data: break
         print datetime.now(), "\n", data
-        tcpCliSoc.send("Welcome to the server")
-        #tcpCliSoc.send('Welcome to the server! ',time.strftime("%a %b %d %H:%M:%S", time.localtime()))
+        # time.sleep(1)
+        try:
+            tcpCliSoc.send("Welcome to the server, message:\n[" + data + "]\nReceived successfully!")
+        except socket.error, msg:
+            print 'Socket Error! send' + str(msg[0] + ', Error message:' + msg[1])
+            sys.exit()
+        except Exception, errcode:
+            if errcode[0] == 10035:
+                print 'Error Code:10035'
+            if errcode[0] == 'timed out':
+                print 'Time out'
+
     tcpCliSoc.close()
     print addr, ' close the connection.'
 tcpSerSoc.close()
