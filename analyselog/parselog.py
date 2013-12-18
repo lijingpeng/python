ff16a9cb1558b65576d3a5049bbe816fef42fd26
lijingpeng <lijingpeng@outlook.com>
2013-12-18 17:01:37 +0800

    store opponent actions

--- analyselog/parselog.py
+++ analyselog/parselog.py
@@ -15,16 +15,21 @@ Champion = "marv"
 logFiles = os.listdir( LogFolder )
 
 fwrite = open(OutputFile, "w")
+fwrite.write("line sb win opokers actions oactions\n")
 dataOutput = ""
+
 # open files and parse it
 fileCount = 0
 for logfile in logFiles:
     fileCount += 1
     print "Parsing file ", fileCount
     fread = open( LogFolder + logfile )
+    lineCount = 0
     dataLine = fread.readline()
     while dataLine:
         if dataLine[0] != "#":
+            lineCount += 1
+            dataOutput += str(lineCount) + "#"
             # main process here
             dataPiece = dataLine.split(":")
             # STATE:0:rc/crc/crc/crc:Tc6d|Ks5h/QsTs2s/9s/Ah:-70|70:feste_iro|marv
@@ -44,11 +49,13 @@ for logfile in logFiles:
                 championIndex = 0
             else:
                 championIndex = 1
+            # store the champion, small blind or big blind
+            dataOutput += str(championIndex) + "#"
 
             # if the champion is the winner
             if championIndex == winnerIndex:
                 dataOutput += "1"
-            print "winner ", winnerIndex
+            #print "winner ", winnerIndex
 
             # get all the pokers into pokers
             pokers_a = dataPiece[3].split("|")
@@ -58,6 +65,32 @@ for logfile in logFiles:
             dataOutput += "#" + pokers[championIndex]
             for i in range(2, len(pokers), 1):
                 dataOutput += "|" + pokers[i]
+
+            # get actions
+            dataOutput += "#" + dataPiece[2] + "#"
+            actions = dataPiece[2].split("/")
+
+            # opponent's action sequence
+            oppoActions = ""
+            listTmp = list(actions[0])
+            # the champion is the small blind, he goes first
+            for i in range(championIndex,len(listTmp), 2):
+                oppoActions += listTmp[i]
+            # after pre-flop, the action sequence is changed
+            for i in range(1 , len(actions), 1):
+                oppoActions += "|"
+                listTmp = list(actions[i])
+                # the champion is the small blind, he goes first
+                if championIndex == 1:
+                    for i in range(0,len(listTmp), 2):
+                        oppoActions += listTmp[i]
+                else:
+                    for i in range(1,len(listTmp), 2):
+                        oppoActions += listTmp[i]
+
+            # Store all the opponent's actions
+            dataOutput += oppoActions
+
             #print dataPiece[2] # action
             #print dataPiece[3] # poker
             #print dataPiece[4] # pot
