d3b00ed0247da0ddfc068aa8ee427be4f6a8106f
lijingpeng <lijingpeng@outlook.com>
2013-12-19 16:36:15 +0800

    GetPotAll

--- analyselog/anninput.py
+++ analyselog/anninput.py
@@ -17,6 +17,11 @@ def GetRaiseCount(opActions):
     return Count
     pass
 
+
+def GetPotAll(actions, stopIndex):
+    pass
+
+
 # open output file
 fileWriter = open(annDataFile, "w")
 dataOutput = ""
@@ -44,12 +49,15 @@ while dataLine and lineCount < 10:
     stages = dataPiece[4].split("/")    # All action sequence
     opStages = dataPiece[5].split("|")  # opponent's action sequence
     roundCount = len(stages)
+    # following variables are the input of the neural network
     FLOP = 0
     TURN = 0
     RIVER = 0
     OP_RAISE_COUNT = 0
+    POTALL = 15 # the money in the pot till now, once start the game, it is 15
+    ME_SB = dataPiece[1] # 0 for big blind, 1 for small blind
     for i in range(0, roundCount, 1): # i will be 0,1,2,3; 0 will be valid, while 1,2,3 depends
-        if i == 0: #stage 1 [pre-flop]
+        if i == 0: #stage 1 [pre-flop], FLOP = 0, TURN = 0, RIVER = 0
             FLOP = 0
             TURN = 0
             RIVER = 0
@@ -57,14 +65,6 @@ while dataLine and lineCount < 10:
             print "raise", OP_RAISE_COUNT
         pass
     pass
-
-    # pre-flop stage, FLOP = 0, TURN = 0, RIVER = 0
-
-    # flop stage
-
-    # turn stage
-
-    # river stage
 
     print dataPiece
     dataLine = fileReader.readline()
