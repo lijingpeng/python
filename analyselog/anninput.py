ba5a0546473ad4e01562c3034efd0d4519e397c5
lijingpeng <lijingpeng@outlook.com>
2013-12-19 10:50:03 +0800

    read file

--- analyselog/anninput.py
+++ analyselog/anninput.py
@@ -2,3 +2,18 @@ __author__ = 'lijingpeng'
 
 # Generate ann input
 
+# Global data
+dataFile = "/home/lijingpeng/Public/annout.txt"
+annDataFile = "/home/lijingpeng/Public/ann.txt"
+
+# open data file and processing it
+fileReader = open(dataFile)
+dataLine = fileReader.readline()
+lineCount = 0
+while dataLine and lineCount < 10:
+    # main process here
+    print dataLine
+    lineCount += 1
+    pass
+
+print "----------------------------------------------------\nDone."
